638846fc1838d2ccb368e8f4eaf5f1ea4daa40ef
Aryan Malik <Aryanmalik19.am@gmail.com>
2021-05-01 10:34:27 +0530

    Updated

---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const app = express();
+
+
+app.get('/', (req, res) => { 
+    const data=`<h1><a href ="/hoverboard"> Hkoverboard</a>
+    <br><a href ="/Tictactoe"> TicTacToe</a></h1>`
+    res.send(data)
+})
+
+app.get('/Tictactoe', (req, res) => {
+    res.sendFile(__dirname + '/public/Tictactoe/home.html')
+})
+app.get('/Tictactoe/js', (req, res) => {
+    res.sendFile(__dirname + '/public/Tictactoe/scripts.js')
+})
+
+app.get('/hoverboard', (req, res) => {
+    res.sendFile(__dirname + '/public/hoverboard/home.html')
+})
+
+app.get('/hoverboard/css', (req, res) => {
+    res.sendFile(__dirname + '/public/hoverboard/styles.css')
+})
+
+app.get('/hoverboard/js', (req, res) => {
+    res.sendFile(__dirname + '/public/hoverboard/scripts.js')
+})
+
+app.listen(3000, () => console.log('Server Started at 3000'))
